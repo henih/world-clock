89b5fccc5a3cb5194d8b422d65095e0dd54b3cff
henih <86572062+henih@users.noreply.github.com>
2023-07-13 10:57:07 +0200

    Scripted basic clock automation

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,10 @@
+setInterval(function () {
+  let cityElement = document.querySelector("#city");
+  let dateElement = cityElement.querySelector(".date");
+  let timeElement = cityElement.querySelector(".time");
 
+  dateElement.innerHTML = moment().tz("US/Hawaii").format("MMMM Do YYYY");
+  timeElement.innerHTML = moment()
+    .tz("US/Hawaii")
+    .format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
